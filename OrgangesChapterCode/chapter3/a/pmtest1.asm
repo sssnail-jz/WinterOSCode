5ff66af8a64cf209c59d54bfd4f0f743ba0ecdbe
thinkbookp14 <thinbookp14@temp.com>
2021-08-09 16:53:33 +0800

    注释 chapter3/a/pmtest1.asm

--- OrgangesChapterCode/chapter3/a/pmtest1.asm
+++ OrgangesChapterCode/chapter3/a/pmtest1.asm
@@ -10,19 +10,19 @@ org	07c00h
 
 [SECTION .gdt]
 ; GDT
-;                              段基址,       段界限     , 属性
-LABEL_GDT:	   Descriptor       0,                0, 0           ; 空描述符
-LABEL_DESC_CODE32: Descriptor       0, SegCode32Len - 1, DA_C + DA_32; 非一致代码段
-LABEL_DESC_VIDEO:  Descriptor 0B8000h,           0ffffh, DA_DRW	     ; 显存首地址
+;                              段基址,       段界限, 			属性
+LABEL_GDT:	   		Descriptor 0,            0, 				0           ; 空描述符
+LABEL_DESC_CODE32: 	Descriptor 0, 			 SegCode32Len - 1, 	DA_C + DA_32; 非一致代码段
+LABEL_DESC_VIDEO:  	Descriptor 0B8000h,      0ffffh, 			DA_DRW	   	; 显存首地址
 ; GDT 结束
 
 GdtLen		equ	$ - LABEL_GDT	; GDT长度
-GdtPtr		dw	GdtLen - 1	; GDT界限
-		dd	0		; GDT基地址
+GdtPtr		dw	GdtLen - 1		; GDT界限
+			dd	0				; GDT基地址
 
 ; GDT 选择子
-SelectorCode32		equ	LABEL_DESC_CODE32	- LABEL_GDT
-SelectorVideo		equ	LABEL_DESC_VIDEO	- LABEL_GDT
+SelectorCode32	equ	LABEL_DESC_CODE32 - LABEL_GDT
+SelectorVideo	equ	LABEL_DESC_VIDEO - LABEL_GDT
 ; END of [SECTION .gdt]
 
 [SECTION .s16]
@@ -38,8 +38,10 @@ LABEL_BEGIN:
 	xor	eax, eax
 	mov	ax, cs
 	shl	eax, 4
-	add	eax, LABEL_SEG_CODE32
-	mov	word [LABEL_DESC_CODE32 + 2], ax
+	; #### cs * 16 + 偏移地址，表示 LABEL_SEG_CODE32 的逻辑地址
+	add	eax, LABEL_SEG_CODE32				
+	; #### 将 eax 中的 LABEL_SEG_CODE32 地址填充到 LABEL_DESC_CODE32  描述符
+	mov	word [LABEL_DESC_CODE32 + 2], ax	
 	shr	eax, 16
 	mov	byte [LABEL_DESC_CODE32 + 4], al
 	mov	byte [LABEL_DESC_CODE32 + 7], ah
@@ -48,11 +50,11 @@ LABEL_BEGIN:
 	xor	eax, eax
 	mov	ax, ds
 	shl	eax, 4
-	add	eax, LABEL_GDT		; eax <- gdt 基地址
+	add	eax, LABEL_GDT			; eax <- gdt 基地址
 	mov	dword [GdtPtr + 2], eax	; [GdtPtr + 2] <- gdt 基地址
 
 	; 加载 GDTR
-	lgdt	[GdtPtr]
+	lgdt [GdtPtr]
 
 	; 关中断
 	cli
@@ -69,19 +71,19 @@ LABEL_BEGIN:
 
 	; 真正进入保护模式
 	jmp	dword SelectorCode32:0	; 执行这一句会把 SelectorCode32 装入 cs,
-					; 并跳转到 Code32Selector:0  处
+								; 并跳转到 Code32Selector:0  处
 ; END of [SECTION .s16]
 
 
-[SECTION .s32]; 32 位代码段. 由实模式跳入.
+[SECTION .s32]					; 32 位代码段. 由实模式跳入.
 [BITS	32]
 
 LABEL_SEG_CODE32:
 	mov	ax, SelectorVideo
-	mov	gs, ax			; 视频段选择子(目的)
+	mov	gs, ax					; 视频段选择子(目的)
 
 	mov	edi, (80 * 11 + 79) * 2	; 屏幕第 11 行, 第 79 列。
-	mov	ah, 0Ch			; 0000: 黑底    1100: 红字
+	mov	ah, 0Ch					; 0000: 黑底    1100: 红字
 	mov	al, 'P'
 	mov	[gs:edi], ax
 
@@ -90,4 +92,3 @@ LABEL_SEG_CODE32:
 
 SegCode32Len	equ	$ - LABEL_SEG_CODE32
 ; END of [SECTION .s32]
-
