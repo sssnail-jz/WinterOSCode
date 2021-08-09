ac97ad0b8711eeda15ded5383385f6dd4cb13d60
thinkbookp14 <thinbookp14@temp.com>
2021-08-09 20:50:23 +0800

    修改 chapter3/b/pmtest2.asm

--- OrgangesChapterCode/chapter3/b/pmtest2.asm
+++ OrgangesChapterCode/chapter3/b/pmtest2.asm
@@ -3,27 +3,27 @@
 ; 编译方法：nasm pmtest2.asm -o pmtest2.com
 ; ==========================================
 
-%include	"pm.inc"	; 常量, 宏, 以及一些说明
+%include "pm.inc"	; 常量, 宏, 以及一些说明
 
 	org	0100h
 	jmp	LABEL_BEGIN
 
 [SECTION .gdt]
 ; GDT
-;                            段基址,        段界限 , 属性
-LABEL_GDT:         Descriptor    0,              0, 0         ; 空描述符
-LABEL_DESC_NORMAL: Descriptor    0,         0ffffh, DA_DRW    ; Normal 描述符
-LABEL_DESC_CODE32: Descriptor    0, SegCode32Len-1, DA_C+DA_32; 非一致代码段, 32
-LABEL_DESC_CODE16: Descriptor    0,         0ffffh, DA_C      ; 非一致代码段, 16
-LABEL_DESC_DATA:   Descriptor    0,      DataLen-1, DA_DRW    ; Data
-LABEL_DESC_STACK:  Descriptor    0,     TopOfStack, DA_DRWA+DA_32; Stack, 32 位
-LABEL_DESC_TEST:   Descriptor 0500000h,     0ffffh, DA_DRW
-LABEL_DESC_VIDEO:  Descriptor  0B8000h,     0ffffh, DA_DRW    ; 显存首地址
+;                            	段基址,      段界限 , 		属性
+LABEL_GDT:         Descriptor    0,         0, 				0         		; 空描述符
+LABEL_DESC_NORMAL: Descriptor    0,         0ffffh, 		DA_DRW    		; Normal 描述符
+LABEL_DESC_CODE32: Descriptor    0, 		SegCode32Len-1, DA_C+DA_32		; 非一致代码段, 32
+LABEL_DESC_CODE16: Descriptor    0,         0ffffh, 		DA_C      		; 非一致代码段, 16
+LABEL_DESC_DATA:   Descriptor    0,      	DataLen-1, 		DA_DRW    		; Data
+LABEL_DESC_STACK:  Descriptor    0,     	TopOfStack, 	DA_DRWA+DA_32	; Stack, 32 位
+LABEL_DESC_TEST:   Descriptor 	 0500000h,  0ffffh, 		DA_DRW
+LABEL_DESC_VIDEO:  Descriptor  	 0B8000h,   0ffffh, 		DA_DRW    		; 显存首地址
 ; GDT 结束
 
 GdtLen		equ	$ - LABEL_GDT	; GDT长度
-GdtPtr		dw	GdtLen - 1	; GDT界限
-		dd	0		; GDT基地址
+GdtPtr		dw	GdtLen - 1		; GDT界限
+			dd	0				; GDT基地址
 
 ; GDT 选择子
 SelectorNormal		equ	LABEL_DESC_NORMAL	- LABEL_GDT
@@ -41,11 +41,11 @@ ALIGN	32
 LABEL_DATA:
 SPValueInRealMode	dw	0
 ; 字符串
-PMMessage:		db	"In Protect Mode now. ^-^", 0	; 在保护模式中显示
+PMMessage:			db	"In Protect Mode now. ^-^", 0	; 在保护模式中显示
 OffsetPMMessage		equ	PMMessage - $$
-StrTest:		db	"ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0
+StrTest:			db	"ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0
 OffsetStrTest		equ	StrTest - $$
-DataLen			equ	$ - LABEL_DATA
+DataLen				equ	$ - LABEL_DATA
 ; END of [SECTION .data1]
 
 
@@ -57,7 +57,6 @@ LABEL_STACK:
 	times 512 db 0
 
 TopOfStack	equ	$ - LABEL_STACK - 1
-
 ; END of [SECTION .gs]
 
 
@@ -117,11 +116,11 @@ LABEL_BEGIN:
 	xor	eax, eax
 	mov	ax, ds
 	shl	eax, 4
-	add	eax, LABEL_GDT		; eax <- gdt 基地址
+	add	eax, LABEL_GDT			; eax <- gdt 基地址
 	mov	dword [GdtPtr + 2], eax	; [GdtPtr + 2] <- gdt 基地址
 
 	; 加载 GDTR
-	lgdt	[GdtPtr]
+	lgdt [GdtPtr]
 
 	; 关中断
 	cli
@@ -137,7 +136,8 @@ LABEL_BEGIN:
 	mov	cr0, eax
 
 	; 真正进入保护模式
-	jmp	dword SelectorCode32:0	; 执行这一句会把 SelectorCode32 装入 cs, 并跳转到 Code32Selector:0  处
+	jmp	dword SelectorCode32:0	; 执行这一句会把 SelectorCode32 装入 cs, 
+								; 并跳转到 Code32Selector:0  处
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -149,14 +149,14 @@ LABEL_REAL_ENTRY:		; 从保护模式跳回到实模式就到了这里
 
 	mov	sp, [SPValueInRealMode]
 
-	in	al, 92h		; `.
+	in	al, 92h			; `.
 	and	al, 11111101b	;  | 关闭 A20 地址线
-	out	92h, al		; /
+	out	92h, al			; /
 
-	sti			; 开中断
+	sti					; 开中断
 
-	mov	ax, 4c00h	; `.
-	int	21h		; /  回到 DOS
+	mov	ax, 4c00h		; `.
+	int	21h				; /  回到 DOS
 ; END of [SECTION .s16]
 
 
@@ -165,20 +165,20 @@ LABEL_REAL_ENTRY:		; 从保护模式跳回到实模式就到了这里
 
 LABEL_SEG_CODE32:
 	mov	ax, SelectorData
-	mov	ds, ax			; 数据段选择子
+	mov	ds, ax					; 数据段选择子
 	mov	ax, SelectorTest
-	mov	es, ax			; 测试段选择子
+	mov	es, ax					; 测试段选择子
 	mov	ax, SelectorVideo
-	mov	gs, ax			; 视频段选择子
+	mov	gs, ax					; 视频段选择子
 
 	mov	ax, SelectorStack
-	mov	ss, ax			; 堆栈段选择子
+	mov	ss, ax					; 堆栈段选择子
 
 	mov	esp, TopOfStack
 
 
 	; 下面显示一个字符串
-	mov	ah, 0Ch			; 0000: 黑底    1100: 红字
+	mov	ah, 0Ch					; 0000: 黑底    1100: 红字
 	xor	esi, esi
 	xor	edi, edi
 	mov	esi, OffsetPMMessage	; 源数据偏移
@@ -317,11 +317,14 @@ LABEL_SEG_CODE16:
 	mov	gs, ax
 	mov	ss, ax
 
+	; 清楚 cr0 的保护模式位，进入实模式
 	mov	eax, cr0
 	and	al, 11111110b
 	mov	cr0, eax
 
 LABEL_GO_BACK_TO_REAL:
+	; <2021>当初程序一执行就把实模式下使用的 cs 的值保存在这里了，所以才能够跳回到保护模式,
+	; 所以 cs * 16 + 偏移地址才正确
 	jmp	0:LABEL_REAL_ENTRY	; 段地址会在程序开始处被设置成正确的值
 
 Code16Len	equ	$ - LABEL_SEG_CODE16
